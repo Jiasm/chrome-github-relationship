57fcdb274e779e40057179af39d55b1e0eacc4e4
jiashunming <jiashunming@outlook.com>
2018-05-05 16:02:27 +0800

    update: console version - done

--- libs/popup.js
+++ libs/popup.js
@@ -4,9 +4,12 @@ let $navItems = document.querySelectorAll('.nav-item')
 let $accounts = document.querySelectorAll('.account')
 let $tokens = document.querySelectorAll('.token')
 let $generates = document.querySelectorAll('.generate')
+let $removes = document.querySelectorAll('.remove')
 
 // default active panel
 let cursorPanel
+// add of remove token switch
+let tokenStatus = false
 // form data
 let formData = {}
 
@@ -16,9 +19,31 @@ init()
  * initialize
  */
 async function init () {
+  tokenStatus = Boolean(await getStorage('github_auth_token'))
   bindEvents()
-  cursorPanel = await getCursorPanel() || 'token'
+  cursorPanel = await getStorage('cursor_nav') || 'token'
   activePanel(cursorPanel)
+  changeRemoveButton(cursorPanel)
+  await fillInputFromStorage()
+}
+
+async function fillInputFromStorage () {
+  let cursor = await getStorage('cursor_nav')
+
+  if (cursor) {
+    let account = await getStorage('github_user_account') || ''
+    let token = await getStorage('github_user_token') || ''
+
+    document.querySelector(`.account[data-type="${cursor}"]`).value = account
+    document.querySelector(`.token[data-type="${cursor}"]`).value = token
+    // fill formData
+    formData[cursor] = {
+      account,
+      token
+    }
+
+    changeButtonStatus()
+  }
 }
 
 /**
@@ -26,49 +51,9 @@ async function init () {
  */
 function bindEvents () {
   addEvents($navItems, 'click', changeNavHandler)
-  addEvents($accounts, 'keyup', keyupHandler)
-  addEvents($tokens, 'keyup', keyupHandler)
+  addEvents([].concat(...$accounts, ...$tokens), 'keyup', keyupHandler)
   addEvents($generates, 'click', generateHandler)
-
-  function changeNavHandler (e) {
-    let {target} = e
-
-    let {type} = target.dataset
-
-    if (type !== cursorPanel) {
-      cursorPanel = type
-      activePanel(cursorPanel)
-    }
-  }
-
-  function keyupHandler (e) {
-    let {target} = e
-
-    formData[cursorPanel][target.dataset.type] = target.value
-
-    buttonStatus()
-  }
-
-  function buttonStatus () {
-    let form = formData[cursorPanel]
-    let $generate = document.querySelector(`.generate[data-type="${cursorPanel}"]`)
-    if (form.account && form.token) {
-      $generate.classList.remove('disabled')
-    } else {
-      $generate.classList.add('disabled')
-    }
-  }
-
-  function generateHandler () {
-    let form = formData[cursorPanel]
-    if (form.account && form.token) {
-      let token = `Basic ${window.btoa(`${formData.account}:${formData.token}`)}`
-
-      // chrome.storage.local.set({'github_auth_token': token})
-
-      alert('generate success')
-    }
-  }
+  addEvents($removes, 'click', removeHandler)
 }
 
 /**
@@ -78,9 +63,9 @@ function addEvents (tags, event, handler) {
   tags.forEach(tag => tag && event && handler && tag.addEventListener(event, handler))
 }
 
-function removeEvents (tags, event, handler) {
-  tags.forEach(tag => tag && event && handler && tag.removeEventListener(event, handler))
-}
+// function removeEvents (tags, event, handler) {
+//   tags.forEach(tag => tag && event && handler && tag.removeEventListener(event, handler))
+// }
 
 /**
  * active a panel
@@ -92,9 +77,13 @@ function activePanel (cursor) {
   hidePanels()
   $cursorPanel.classList.add('active')
   $cursorNav.classList.add('selected')
-  chrome.storage.local.set({'cursor_nav': cursor})
+  setStorage('cursor_nav', cursor)
   // init formData
   formData[cursor] = formData[cursor] || {}
+  // focus input
+  document.querySelector(`.account[data-type="${cursor}"]`).focus()
+
+  changeButtonStatus()
 }
 
 /**
@@ -105,11 +94,98 @@ function hidePanels () {
   $navItems.forEach($item => $item && $item.classList.remove('selected'))
 }
 
-async function getCursorPanel () {
+function changeNavHandler (e) {
+  let {target} = e
+
+  let {type} = target.dataset
+
+  if (type !== cursorPanel) {
+    cursorPanel = type
+    activePanel(cursorPanel)
+  }
+}
+
+function keyupHandler (e) {
+  let {target} = e
+
+  formData[cursorPanel][target.dataset.field] = target.value
+
+  console.log(formData)
+
+  changeButtonStatus()
+}
+
+function changeButtonStatus () {
+  let form = formData[cursorPanel]
+  let $generate = document.querySelector(`.generate[data-type="${cursorPanel}"]`)
+  if (form.account && form.token) {
+    $generate.classList.remove('disabled')
+  } else {
+    $generate.classList.add('disabled')
+  }
+}
+
+function changeRemoveButton (cursor) {
+  let $generate = document.querySelector(`.generate[data-type="${cursor}"]`)
+  let $remove = document.querySelector(`.remove[data-type="${cursor}"]`)
+  if (tokenStatus) {
+    $generate.classList.add('hide')
+    $remove.classList.add('active')
+  } else {
+    $generate.classList.remove('hide')
+    $remove.classList.remove('active')
+  }
+}
+
+/**
+ * generate token
+ */
+function generateHandler (e) {
+  let {target} = e
+
+  if (target.classList.contains('disabled')) return
+
+  let form = formData[cursorPanel]
+  if (form.account && form.token) {
+    let token = `Basic ${window.btoa(`${form.account}:${form.token}`)}`
+
+    setStorage('github_user_account', form.account)
+    setStorage('github_user_token', form.token)
+    setStorage('github_auth_token', token)
+
+    // set btn with success
+    tokenStatus = true
+    // change dom
+    changeRemoveButton(cursorPanel)
+  }
+}
+
+function removeHandler (e) {
+  let {target} = e
+
+  if (target.classList.contains('disabled')) return
+
+  // clear memory
+  formData[cursorPanel] = {}
+  tokenStatus = false
+
+  // clear storage
+  setStorage('github_user_account', null)
+  setStorage('github_user_token', null)
+  setStorage('github_auth_token', null)
+
+  // change dom
+  document.querySelector(`.account[data-type="${cursorPanel}"]`).value = ''
+  document.querySelector(`.token[data-type="${cursorPanel}"]`).value = ''
+  changeRemoveButton(cursorPanel)
+  changeButtonStatus()
+}
+
+async function getStorage (key) {
   return new Promise((resolve, reject) => {
-    chrome.storage.local.get('cursor_nav', data => {
-      if (data.cursor_nav) {
-        resolve(data.cursor_nav)
+    chrome.storage.local.get(key, data => {
+      if (data[key]) {
+        resolve(data[key])
       } else {
         resolve()
       }
@@ -117,10 +193,9 @@ async function getCursorPanel () {
   })
 }
 
-// chrome.storage.local.set({'github_auth_token': 'test'})
-//
-// chrome.storage.local.get('github_auth_token', data => {
-//   if (data.address) {
-//     document.querySelector('#username').value = data.address
-//   }
-// })
+async function setStorage (key, value) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.set({[key]: value})
+    resolve()
+  })
+}
--- libs/relationship.js
+++ libs/relationship.js
@@ -1,4 +1,6 @@
-async function test () {
+let current = {}
+
+async function main () {
   try {
     let meta = document.querySelector('meta[name="octolytics-actor-login"]')
 
@@ -9,7 +11,7 @@ async function test () {
     }
 
     let currentUser = meta.content
-    let token = await getToken()
+    let token = await getStorage('github_auth_token')
 
     if (!token) return console.log('has no token')
 
@@ -22,23 +24,121 @@ async function test () {
     if (result.status === 200) {
       let data = await result.json()
 
-      console.log(data)
+      current = data
+      current.token = token
+
+      console.log(`welcome ${current.login}!`, location.href)
+    }
+
+    bindEvents()
+
+    let urlReg = /\?tab=(followers|following)/i
+    if (urlReg.test(location.search)) {
+      await tabStatusHandler(urlReg.exec(location.search)[1])
     }
   } catch (e) {
     console.error(e)
   }
 }
 
-test()
+main()
 
-function getToken () {
+function bindEvents () {
+  let $followerNav = document.querySelector('.user-profile-nav a[title="Followers"]')
+  let $followingNav = document.querySelector('.user-profile-nav a[title="Following"]')
+
+  addEvents([].concat($followerNav, $followingNav), 'click', tabStatusHandler)
+}
+
+async function tabStatusHandler (e) {
+  let tabType
+  // trigger by self
+  if (typeof e === 'string') {
+    tabType = e
+  } else {
+    tabType = e.target.title
+  }
+
+  tabType = tabType.toLowerCase()
+
+  let $list = document.querySelectorAll('.position-relative>div.d-table')
+
+  let results = {}
+
+  await Promise.all(Array.from($list).map(async function (item) {
+    // slice from `@`
+    let target = item.querySelector('.avatar').alt.slice(1)
+
+    if (tabType === 'followers') {
+      // your followers list
+      let result = await fetch(`https://api.github.com/users/${current.login}/following/${target}`, {
+        credentials: 'same-origin',
+        headers: {
+          Authorization: current.token
+        }
+      })
+      console.clear()
+      if (result.status === 204) {
+        results[target] = true
+        // you has follow him
+        // console.log(`you has follow ${target}`)
+      } else if (result.status === 404) {
+        // you has not follow him
+        // console.log(`you has not follow ${target}`)
+        results[target] = false
+      }
+    } else if (tabType === 'following') {
+      // your followling list
+      let result = await fetch(`https://api.github.com/users/${target}/following/${current.login}`, {
+        credentials: 'same-origin',
+        headers: {
+          Authorization: current.token
+        }
+      })
+      console.clear()
+      if (result.status === 204) {
+        // he has follow you
+        results[target] = true
+        // console.log(`${target} has follow you`)
+      } else if (result.status === 404) {
+        // he has not follow you
+        results[target] = false
+        // console.log(`${target} has not follow you`)
+      }
+    }
+  }))
+
+  Object.entries(results).map(([name, status]) => {
+    if (tabType === 'followers') {
+      console.log(`you has has ${status ? '   ' : 'not'} follow ${name}`)
+    } else if (tabType === 'following') {
+      console.log(`${name.padEnd(16, ' ')} has ${status ? '   ' : 'not'} follow you`)
+    }
+  })
+}
+
+/**
+ * a jquery like event listener
+ */
+function addEvents (tags, event, handler) {
+  tags.forEach(tag => tag && event && handler && tag.addEventListener(event, handler))
+}
+
+async function getStorage (key) {
   return new Promise((resolve, reject) => {
-    chrome.storage.local.get('github_auth_token', data => {
-      if (data.address) {
-        resolve(data.address)
+    chrome.storage.local.get(key, data => {
+      if (data[key]) {
+        resolve(data[key])
       } else {
         resolve()
       }
     })
   })
 }
+
+// async function setStorage (key, value) {
+//   return new Promise((resolve, reject) => {
+//     chrome.storage.local.set({[key]: value})
+//     resolve()
+//   })
+// }
